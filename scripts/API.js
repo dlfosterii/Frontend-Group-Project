82bf8fc9425872a2aa626a7a0410ea97061ca416
David Foster <dlfosterii@yahoo.com>
2020-05-05 15:48:58 -0400

    saved for getting new master

--- scripts/API.js
+++ scripts/API.js
@@ -1,20 +1,43 @@
 
-// let search = 
-
+let newsSearch = []
+let newsList = document.getElementById('news')
 
 
 axios.get('https://gnews.io/api/v3/search?q=georgia&token=a5ad6e266f09e9d9a306372a761e63bd')
 .then(function (response){
-    console.log(response)
-    console.log(response.data)
-    console.log(response.data.articles)
-    response.data.articles.forEach(article => {
-        console.log(article.title)
+    newsSearch = response.data
+    // console.log(response)
+    // console.log(response.data)
+    // console.log(response.data.articles)
+    newsSearch.articles.map(function (article => {
+        // console.log(article.title)
+        new
         
     });
 })
 
-// console.log(axios.get('https://gnews.io/api/v3/search?q=georgia&token=a5ad6e266f09e9d9a306372a761e63bd'))
 
-// console.log('hello')
+function displayStories (array) {
 
+}
+
+
+// function renderMovies(movieArray) {
+//     let movieHtmlArray = movieArray.map(function (currentMovie) {
+//         return `
+//         <div id="movie" class="card m-3" style="width: 18rem;">
+//         <img id="poster"
+//             src="${currentMovie.Poster}"
+//             class="card-img-top" alt="${currentMovie.Title}">
+//         <div class="card-body">
+//             <div class="titleDate">
+//             <h5 id="title" class="card-title">${currentMovie.Title}</h5>
+//             <h4 id="releaseDate"><span class="badge badge-secondary">${currentMovie.Year}</span></h4v>
+//             </div>
+//             <a href="#" class="btn btn-primary" onclick="saveToWatchlist('${currentMovie.imdbID}')">Add to Watchlist!</a>
+//         </div>
+//         </div>`
+//     }
+//     )
+//     return movieHtmlArray.join('');
+// };
