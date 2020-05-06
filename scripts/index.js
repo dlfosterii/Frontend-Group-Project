f3760a8d8dd20855853e0a2785f7a172bba21972
Nicholas Nam <admin@nicholasnam.com>
2020-05-06 08:49:47 -0400

    Begin new and returning user check
    On page load, the JS function will execute to see if user has visited before.

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,17 @@
-function customPage() {
+document.addEventListener("DOMContentLoaded", function(){
+    pageInit();
+});
+
+function pageInit() {
     var x = document.getElementById("newUser");
 
     if (x.style.display = "none") {
         x.style.display = "flex";
+
         console.log("WENT TO IF");
     } else {
         x.style.display = "none";
       console.log("WENT TO ELSE");
     }
 }
+
