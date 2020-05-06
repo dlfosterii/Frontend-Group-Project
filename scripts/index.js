3b48346f42484a2354ac7b29e50f671e18835706
Nicholas Nam <admin@nicholasnam.com>
2020-05-06 10:23:33 -0400

    Hide newUser div in index.html
    Updated code to not show newUser div in the index.html

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,12 @@
 document.addEventListener("DOMContentLoaded", function(){
+    var visited = localStorage
     pageInit();
 });
 
 function pageInit() {
     var x = document.getElementById("newUser");
 
-    if (x.style.display = "none") {
+    if () {
         x.style.display = "flex";
 
         console.log("WENT TO IF");
