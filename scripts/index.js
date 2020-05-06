54bf0ecdb0b5d5984526e98ff54ec2bed05c48e4
Nicholas Nam <admin@nicholasnam.com>
2020-05-06 13:14:11 -0400

    Working on code for page loading
    Loading page code is almost complete. Needs Jon's API code for location and cookie.

--- scripts/index.js
+++ scripts/index.js
@@ -1,18 +1,29 @@
-document.addEventListener("DOMContentLoaded", function(){
-    var visited = localStorage
-    pageInit();
-});
+document.addEventListener('DOMContentLoaded', function() {
 
-function pageInit() {
-    var x = document.getElementById("newUser");
 
-    if () {
-        x.style.display = "flex";
+    //var visited = 
+    localStorage.setItem('user', "city: 'atlanta', zip: '30043'");    //localStorage.getItem('user');
+    let visited = localStorage.getItem('user');
+    var returnDiv = document.getElementById('returningUser');
+    cons
 
-        console.log("WENT TO IF");
-    } else {
-        x.style.display = "none";
-      console.log("WENT TO ELSE");
+    // get users info from local storage
+
+    //const userLocation = localStorage.setItem('user', {city: 'atlanta', zip: '30043'});    //localStorage.getItem('user');//JSON.parse(visited);
+    pageInit(visited);
+
+    function pageInit(visitStatus) {
+        var x = document.getElementById('newUser');
+    
+        // NEED TO PUT OTHER NOT-TRUE CONDITIONS BELOW
+        if (visitStatus != null) {
+            x.style.display = 'none';
+            console.log(visitStatus.user.city);
+           // console.log(returnDiv.textContent += `${visitStatus.city}`);
+    
+        } else {
+            x.style.display = 'flex';
+            x.innerHTML += '<h1>NEW USER</h1>';
+        }
     }
-}
-
+});
