bc27f333a41fd7fd377ed1ae1845e4e1dd5248ba
Jonathan Cox <cox.jonathan@gmail.com>
2020-05-06 15:53:07 -0400

    adds functionality to render news articles onto index.html

--- scripts/API.js
+++ scripts/API.js
@@ -3,12 +3,26 @@ let $newsContainer = $('#news');
 let newsSearch = [];
 
 
-document.addEventListener('DOMContentLoaded', function () {
-    axios.get('https://gnews.io/api/v3/search?q=california&token=3d5ec2062b972fa8960ea9159c358dd6')
-    .then(function (response){
-        newsSearch = response.data.articles;
-        console.log(newsSearch);
-        let newsBlock = newsSearch.map(function (article) {
+// Get user's visit status from local storage
+let user1 = localStorage.getItem('user');
+console.log(user);
+let user1JSON = JSON.parse(user1);
+console.log(user1JSON);
+
+
+let searchCity = user1JSON.city;
+let searchState = user1JSON.state;
+console.log(searchState);
+
+
+
+
+function getNews() {
+  axios.get(`https://gnews.io/api/v3/search?q=${searchCity}%20${searchState}&token=3d5ec2062b972fa8960ea9159c358dd6`)
+    .then(function (response) {
+      newsSearch = response.data.articles;
+      console.log(newsSearch);
+      let newsBlock = newsSearch.map(function (article) {
         console.log(article.title);
         console.log(article.url);
         //Set card into the news container on main page
@@ -25,7 +39,11 @@ document.addEventListener('DOMContentLoaded', function () {
                     </div>
                   </div>
         `
-    })
-    $newsContainer.html(newsBlock)
-        });
-    })
+      })
+      $newsContainer.html(newsBlock);
+      console.log('news block');
+      console.log(newsBlock);
+    });
+}
+
+
