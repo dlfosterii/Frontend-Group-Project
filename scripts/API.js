932f4f2d7e6520be459455c0a1ed31a3b382ed02
Jonathan Cox <cox.jonathan@gmail.com>
2020-05-06 21:20:30 -0400

    working on debugging

--- scripts/API.js
+++ scripts/API.js
@@ -3,32 +3,34 @@ let $newsContainer = $('#news');
 let newsSearch = [];
 
 
-// Get user's visit status from local storage
-let user1 = localStorage.getItem('user');
-console.log(user);
-let user1JSON = JSON.parse(user1);
-console.log(user1JSON);
-
-
-let searchCity = user1JSON.city;
-let searchState = user1JSON.state;
-console.log(searchState);
 
 
 
 
 function getNews() {
-  axios.get(`https://gnews.io/api/v3/search?q=${searchCity}%20${searchState}&token=3d5ec2062b972fa8960ea9159c358dd6`)
+  // Get user's visit status from local storage
+  let user1 = localStorage.getItem('user');
+  console.log(user);
+  let user1JSON = JSON.parse(user1);
+  console.log(user1JSON);
+
+
+  let searchCity = user1JSON.city;
+  let searchState = user1JSON.state;
+  console.log(searchState);
+
+  axios.get(`https://gnews.io/api/v3/search?q=${searchCity}%20${searchState}&token=134a593a2e5be5221726bf39ef418260`)
     .then(function (response) {
       newsSearch = response.data.articles;
       console.log(newsSearch);
       let newsBlock = newsSearch.map(function (article) {
         console.log(article.title);
         console.log(article.url);
+
         //Set card into the news container on main page
         return `
         
-<div class="row row-cols-1 row-cols-md-2">
+<div class="row row-cols-1">
                 <div class="col mb-4">
                   <div class="card">
                     <img src="${article.image}" class="card-img-top" alt="">
@@ -41,8 +43,6 @@ function getNews() {
         `
       })
       $newsContainer.html(newsBlock);
-      console.log('news block');
-      console.log(newsBlock);
     });
 }
 
--- scripts/index.js
+++ scripts/index.js
@@ -1,27 +1,79 @@
 
 document.addEventListener('DOMContentLoaded', function () {
-
-
     // Get user's visit status from local storage
     let visited = localStorage.getItem('user');
-    console.log(visited);
     let visitedJSON = JSON.parse(visited);
-    console.log(visitedJSON);
-
 
     // Assign contants to returningUser and newUser <div> elements in index.html
     const returnDiv = document.getElementById('returningUser');
     const newDiv = document.getElementById('newUser');
+    if (visitedJSON == null) {
+        console.log('localStorage = null');
+        $('#news').hide();
+        $('.search-container').show();
+        $('#navigation').css('display', 'none')
 
-    if (visitedJSON === null) {
-        console.log('null');
     } else {
-        $('.search-container').hide();
+        $('.search-container').css('display', 'none');
         getNews();
     }
+
+    // Event listeners for main search bar button
+    $('#search-button').on('click', function () {
+        const searchValue = $('#search');
+        setLocation(searchValue);
+    });
+    // Event listener for top nav bar button
+    $('#button-nav').on('click', function () {
+        const navValue = $('#search-nav');
+        setLocation(navValue);
+    });
+
+    // Main function to set users Location
+    function setLocation(inputValue) {
+        const searchValue = inputValue.val();
+        const request = {
+            query: searchValue,
+            fields: [
+                'name',
+                'formatted_address',
+            ],
+        };
+        placesService.findPlaceFromQuery(request, function (results, status) {
+            // If else checks user input, can be updated for better error handling
+            if (results == null || results == 'ZERO_RESULTS') {
+                console.log('error with user input');
+            } else {
+                // Formats the response from google API
+                let formattedString = results[0].formatted_address;
+                let locationArray = formattedString.split(', ');
+                let userStateZip = (locationArray[locationArray.length - 2]);
+                let stateZip = userStateZip.split(' ');
+                // Sets the values of the user object 
+                user.state = stateZip[0];
+                user.zip = stateZip[1];
+                user.country = (locationArray[locationArray.length - 1]);
+                user.city = (locationArray[locationArray.length - 3])
+            }
+
+            // Push user {} to localStorage
+            let userInfo = localStorage.getItem('user');
+            let userInfoParsed = JSON.parse(userInfo);
+            // If user doesn't exist in localStorage create empty array
+            if (userInfoParsed === null) {
+                userInfoParsed = [];
+            }
+            // Stringify user then save it to localStorage
+            userInfoToParse = JSON.stringify(user);
+            localStorage.setItem('user', userInfoToParse);
+
+            // Assign users location to the placeholder of Navbar Input
+            $('#search-nav').attr()
+        });
+    }
+
 });
 
-console.log('test');
 // Create global variable for location data
 var placesService;
 
@@ -34,51 +86,6 @@ const user = {
     country: '',
 };
 
-// Event handler that saves user's location input
-$('#search-button').on('click', function () {
-    const searchValue = $('#search').val();
-    const request = {
-        query: searchValue,
-        fields: [
-            'name',
-            'formatted_address',
-        ],
-    };
-    console.log(searchValue);
-    placesService.findPlaceFromQuery(request, function (results, status) {
-        // If else checks user input, can be updated for better error handling
-        if (results == null || results == 'ZERO_RESULTS') {
-            console.log('error with user input');
-        } else {
-            console.log(results, status);
-            // Formats the response from google API
-            let formattedString = results[0].formatted_address;
-            let locationArray = formattedString.split(', ');
-            let userStateZip = (locationArray[locationArray.length - 2]);
-            let stateZip = userStateZip.split(' ');
-            // Sets the values of the user object 
-            user.state = stateZip[0];
-            user.zip = stateZip[1];
-            user.country = (locationArray[locationArray.length - 1]);
-            user.city = (locationArray[locationArray.length - 3])
-            console.log(user);
-        }
-
-        // Push user {} to localStorage
-        let userInfo = localStorage.getItem('user');
-        let userInfoParsed = JSON.parse(userInfo);
-        // If user doesn't exist in localStorage create empty array
-        if (userInfoParsed === null) {
-            userInfoParsed = [];
-            // Stringify user then save it to localStorage
-            userInfoToParse = JSON.stringify(user);
-            console.log(userInfoToParse);
-            localStorage.setItem('user', userInfoToParse);
-        }
-    });
-    newUserDiv();
-});
-
 // Callback function to display returning user's div
 function newUserDiv() {
     newDiv.style.display = 'block';
@@ -90,3 +97,5 @@ function initMap() {
     console.log('init map');
     placesService = new google.maps.places.PlacesService(document.createElement('div'));
 };
+
+
