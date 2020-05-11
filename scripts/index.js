32c6700acc75c10ba9a06bc04afa77e7022119a3
Nicholas Nam <admin@nicholasnam.com>
2020-05-11 13:52:57 -0400

    Update Weather Key
    Testing key

--- scripts/index.js
+++ scripts/index.js
@@ -177,7 +177,7 @@ function getWeather(obj) {
         city = ' ';
     }
     // push in users lat and lng to the weather widget
-    $('#weatherWidget').append(`<div class="climacell-widget" data-apikey="V8bFhHs1eVfWu6jkLVIwmSQc9Nqmpxhh"
+    $('#weatherWidget').append(`<div class="climacell-widget" data-apikey="Ejzw0cBfFqLJY7Cs3pX4ByvMz2Kb3l3y"
     data-type="nowcast" data-location-name="${city}" data-location-lon="${lng}" data-location-lat="${lat}"
     data-size-mode="large" data-font-color="#000" data-background-color="rgba(250,250,250,0)" data-font-family="verdana"
     data-weather-params="temp:F,precipitation:mm/hr,wind_speed:mph,humidity:%,cloud_cover:%"
